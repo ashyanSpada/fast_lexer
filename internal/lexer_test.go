baa364d279f86f54144711e4d5ae0c3654ccefd0
ashyanSpada <ashyan_spada@outlook.com>
2023-11-19 11:29:47 +0800

    feat: add unit tests

--- internal/lexer_test.go
+++ internal/lexer_test.go
@@ -67,14 +67,7 @@ func TestLexer_Next(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := NewLexer(tt.fields.input)
-			l.RegisterToken(
-				new(BoolToken),
-				new(BracketToken),
-				new(NumberToken),
-				new(StringToken),
-				new(IdentifierToken),
-			)
+			l := getTestLexer(tt.fields.input)
 			var tokens []Token
 			for {
 				token, err := l.Next()
@@ -89,4 +82,92 @@ func TestLexer_Next(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestLexer_Peek(t *testing.T) {
+	type fields struct {
+		input string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   Token
+	}{
+		{
+			name: "test",
+			fields: fields{
+				input: "true false True False ( )  { } [ ] -124.123 123e-03 \"ha你好ha\" haha",
+			},
+			want: &BoolToken{
+				source: []rune("true"),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := getTestLexer(tt.fields.input)
+			token, _ := l.Peek()
+			if !reflect.DeepEqual(token, tt.want) {
+				t.Errorf("Lexer.Peek() got = %v, want %v", token, tt.want)
+			}
+		})
+	}
+}
+
+func TestLexer_IsEnd(t *testing.T) {
+	type fields struct {
+		input string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		// TODO: Add test cases.
+		{
+			fields: fields{
+				input: "",
+			},
+			want: true,
+		},
+		{
+			fields: fields{
+				input: "   \t",
+			},
+			want: true,
+		},
+		{
+			fields: fields{
+				input: "   \n",
+			},
+			want: false,
+		},
+		{
+			fields: fields{
+				input: "   \r\n",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			l := getTestLexer(tt.fields.input)
+			if got := l.IsEnd(); got != tt.want {
+				t.Errorf("Lexer.IsEnd() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func getTestLexer(input string) *Lexer {
+	l := NewLexer(input)
+	l.RegisterToken(
+		new(BoolToken),
+		new(BracketToken),
+		new(NumberToken),
+		new(StringToken),
+		new(IdentifierToken),
+	)
+	return l
 }
